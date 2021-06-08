bc74cbd0d440630d6e03694b4de2d72f5cffeae4
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-06-09 02:44:27 +0300

    css skills + js skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,8 +31,26 @@ function linkActive(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkActive))
+
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((element) => {
+    element.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
