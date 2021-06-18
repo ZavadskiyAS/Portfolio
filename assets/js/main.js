c16c10d27a517ad177224404c5f243818cbdcb5d
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-06-19 00:07:54 +0300

    fix scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,11 +147,11 @@ window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
-window.addEventListener('scroll', scrollTop)
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
