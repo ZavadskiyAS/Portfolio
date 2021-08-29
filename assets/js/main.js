1ae52090ddd970ba14baf696a4cb38ed653e4e8b
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-08-30 02:05:54 +0300

    revisions

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,12 +60,12 @@ tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContents=>{
-            tabContents.classList.remove('qualification__active')
+        tabContents.forEach(tabContent=>{
+            tabContent.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
 
-        tab.forEach(tab =>{
+        tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
